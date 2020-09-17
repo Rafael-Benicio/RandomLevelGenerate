24e140e0c874a8d227099d928d1578e513a9218f
Rafael-Benicio <jerobenicio@gmail.com>
2020-09-17 18:29:54 -0300

    Startin and end point

--- World.gd
+++ World.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const Player=preload("res://Player.tscn")
+const Exit=preload("res://ExitDoor.tscn")
 var borders=Rect2(1,1,38,21)
 
 onready var tileMap=$TileMap
@@ -12,12 +14,25 @@ func _ready():
 
 func generate_level():
 	var walker= Walker.new(Vector2(19,21),borders)
-	var map= walker.walk(500)
+	var map= walker.walk(200)
+	
+	var player= Player.instance()
+	add_child(player)
+	player.position=map.front()*32
+	
+	var exit= Exit.instance()
+	add_child(exit)
+	exit.position= walker.get_end_room().position*32
+	exit.connect("leaving_level", self, "reload_level")
+	
 	walker.queue_free()
 	for location in map:
 		tileMap.set_cellv(location,-1)
 	tileMap.update_bitmask_region(borders.position , borders.end)
 
+func reload_level():
+	get_tree().reload_current_scene()
+
 func _input(event):
-	if event.is_action_pressed("ui_right"):
-		get_tree().reload_current_scene()
+	if event.is_action_pressed("ui_accept"):
+		reload_level()
